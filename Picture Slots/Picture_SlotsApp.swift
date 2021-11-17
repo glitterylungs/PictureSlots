4da3f0029c2b06777e73647128c5a0af645bdd21
Alicja Gruca <alicja.gruca6@gmail.com>
2021-11-17 17:54:19 +0100

    Initial Commit

---
+++ Picture Slots/Picture_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picture_SlotsApp.swift
+//  Picture Slots
+//
+//  Created by Alicja Gruca on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Picture_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
